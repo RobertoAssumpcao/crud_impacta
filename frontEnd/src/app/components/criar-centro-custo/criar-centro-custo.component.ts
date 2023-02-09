3612fbecd9b6a7cf376b89c352ad43dfb544c54a
Roberto Assumpção <betoassumpcao@protonmail.com>
2023-02-08 22:34:25 -0300

    form

--- frontEnd/src/app/components/criar-centro-custo/criar-centro-custo.component.ts
+++ frontEnd/src/app/components/criar-centro-custo/criar-centro-custo.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CriarCentroCustoComponent implements OnInit {
 
+  centroCusto = [];
+
   constructor() { }
 
   ngOnInit(): void {
